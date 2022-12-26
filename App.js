4320032d57f7c8d335c86de3d6bb1fd59f91de50
KAVI <kavindu123450@gmail.com>
2022-12-26 19:22:49 +0530

    2

--- App.js
+++ App.js
@@ -1,53 +1,102 @@
 import {Component} from "react";
 import axios from "axios";
 import {
-    View, Text, StyleSheet, TextInput, TouchableOpacity, Platform
+    View, Text, StyleSheet, TextInput, TouchableOpacity, Platform, Modal, Image
 } from "react-native";
 
 export default class App extends Component {
+
+    state={
+        loading:false
+}
     id='';
     name='';
     address='';
     salary=0;
 
     saveCustomer(){
-        axios.post("http://localhost:3000/api/v1/customer/save",{
-            id:this.id,
-            name:this.name,
-            address:this.address,
-            salary:this.salary
-        }).then((resp)=>{
+
+        this.setLoadingState();
+        axios.post("http://127.0.0.1:3000/api/v1/customer/save", {
+            id: this.id,
+            name: this.name,
+            address: this.address,
+            salary: this.salary
+        }).then((resp) => {
             console.log(resp);
-        }).catch(error=>{
+            this.clearTextFields();
+            this.setLoadingState();
+        }).catch(error => {
             console.log(error);
+            this.setLoadingState();
         })
     }
 
+    findCustomer(){
+
+        this.setLoadingState();
+        axios.get("http://127.0.0.1:3000/api/v1/customer/find", {
+            params:{
+                id:this.id
+            }
+        }).then((resp) => {
+            console.log(resp);
+            this.clearTextFields();
+            this.setLoadingState();
+        }).catch(error => {
+            console.log(error);
+            this.setLoadingState();
+        })
+    }
+
+    setLoadingState(){
+        this.setState({loading:!this.state.loading})
+    }
+
+    clearTextFields(){
+        this.id = '';
+        this.name = '';
+        this.address = '';
+        this.salary = 0;
+    }
+
     render() {
+
+        const loading = this.state.loading;
+
         return (
             <View style={styles.body}>
+                <Modal
+                    transparent={true}
+                    visible={loading}
+                    animationType="fade"
+                >
+                    <View style={styles.modal}>
+                        <View style={styles.loading}>
+                            <Image
+                                style={styles.loadingImg}
+                                source='https://wpamelia.com/wp-content/uploads/2018/11/loading-hourglass.gif'/>
+                        </View>
+                    </View>
+                </Modal>
                 <View style={styles.topOuter}>
                     <TextInput
                         onChangeText={(text)=>{this.id=text}}
-                        defaultValue={this.id}
                         style={styles.input}
                         placeholder='Customer Id'
                     />
                     <TextInput
                         onChangeText={(text)=>{this.name=text}}
-                        defaultValue={this.name}
                         style={styles.input}
                         placeholder='Name'
                     />
                     <TextInput
                         onChangeText={(text)=>{this.address=text}}
-                        defaultValue={this.address}
                         style={styles.input}
                         placeholder='Address'
                     />
                     <TextInput
                         onChangeText={(text)=>{this.salary=Number.parseInt(text)}}
-                        defaultValue={this.salary.toString()}
                         style={styles.input}
                         placeholder='Salary'
                     />
@@ -56,7 +105,7 @@ export default class App extends Component {
                         <TouchableOpacity style={styles.button} onPress={()=>{this.saveCustomer()}}>
                             <Text style={styles.buttonText}>Save</Text>
                         </TouchableOpacity>
-                        <TouchableOpacity style={styles.button}>
+                        <TouchableOpacity style={styles.button} onPress={()=>{this.findCustomer()}}>
                             <Text style={styles.buttonText}>Find</Text>
                         </TouchableOpacity>
                         <TouchableOpacity style={styles.button}>
@@ -109,5 +158,23 @@ const styles = StyleSheet.create({
     buttonText: {
         color: 'white',
         fontSize: 15,
+    },
+    modal: {
+        flex:1,
+        backgroundColor:'rgba(44, 62, 80,0.3)',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    loading: {
+        height:200,
+        width:'80%',
+        backgroundColor:'white',
+        borderRadius:20,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    loadingImg: {
+        width: 150,
+        height:'100%'
     }
 })
